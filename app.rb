bea7278f58084cb4c786516c9fa8cd10b154bfd4
Eniehack <studio4units@gmail.com>
2018-04-24 00:22:34 +0900

    converted *.erb to *.haml

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'haml'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
   @title = "home"
-  erb :index
+  haml :index
+end
+
+get '/search' do
+  @title = "検索"
+  @content = "test"
+  haml :search
 end
